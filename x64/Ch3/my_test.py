f64e28a1d37bb7c0b05eee85e93dd9ce8b570a66
BorkoG <borko_3g@mail.com>
2019-05-11 21:36:52 +0100

    Added initial code of chapter 3.

---
+++ x64/Ch3/my_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 11 21:21:41 2019
+
+@author: Borko
+"""
+
+import my_debugger
+
+debug_test = my_debugger.debugger()
+
+debug_test.load("C:\\Windows\\System32\\calc.exe")
+
